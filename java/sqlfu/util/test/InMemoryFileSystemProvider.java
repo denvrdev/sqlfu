2ed09912472788fafaf64f010f27047b5956d2f1
dev <dev@denvr.app>
2018-06-05 00:46:13 -0400

    InMemoryFileSystemProvider added

---
+++ java/sqlfu/util/test/InMemoryFileSystemProvider.java
@@ -0,0 +1,15 @@
+package sqlfu.util.test;
+
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import java.nio.file.FileSystem;
+import javax.inject.Provider;
+
+/** A provider that provides {@link FileSystem} instances for an in-memory file system. */
+public final class InMemoryFileSystemProvider implements Provider<FileSystem> {
+
+  @Override
+  public FileSystem get() {
+    return Jimfs.newFileSystem(Configuration.unix());
+  }
+}
