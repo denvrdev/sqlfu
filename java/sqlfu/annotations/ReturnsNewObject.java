80997b74e6c42b019d2805b5e07f11b9ef1808ca
dev <dev@denvr.app>
2018-06-07 02:10:19 -0400

    ReturnsNewObject added

---
+++ java/sqlfu/annotations/ReturnsNewObject.java
@@ -0,0 +1,20 @@
+package sqlfu.annotations;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * The method to which this annotation applies creates and returns a new object.
+ *
+ * <p>This annotation is typically applied to "factory" methods whose sole responsibility is to
+ * create an instance of a class and return that newly-created instance. The method must not retain
+ * an instance to the created object.
+ */
+@Documented
+@Target(METHOD)
+@Retention(RUNTIME)
+public @interface ReturnsNewObject {}
